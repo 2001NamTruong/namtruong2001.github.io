94deca9f59f82c4dab31efce4c48b054f01d3672
NamTruong2001 <148742251+NamTruong-2001@users.noreply.github.com>
2023-12-14 17:38:55 +0700

    commit

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@
 /* Show window textbox when there is no downloadable file after clicking the button */
 $(document).ready(function() {
     $('.download-cv').click(function() {
-        alert('You are about to access to my CV folder. Click OK to continue');
+        alert('You are about to access to my CV folder. Click OK to continue.');
     });
 
     $('.hire-me').click(function() {
