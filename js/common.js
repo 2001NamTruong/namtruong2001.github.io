bd319f3dd9dc890e15559a0901df63cb740eadb4
NamTruong2001 <148742251+NamTruong-2001@users.noreply.github.com>
2023-12-14 17:34:15 +0700

    commit

--- js/common.js
+++ js/common.js
@@ -3,6 +3,6 @@
 /* Show window textbox when there is no downloadable file after clicking the button */
 $(document).ready(function() {
     $('.download-cv').click(function() {
-        alert('Oops! There is no downloadable file here!');
+        alert('Thank you!');
     });
 });
